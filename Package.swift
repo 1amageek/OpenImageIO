361337a76af8bb68f81f376df76176e421181895
1amageek <tmy0x3@icloud.com>
2025-12-27 14:57:44 +0900

    Update platform versions in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,10 +6,11 @@ import PackageDescription
 let package = Package(
     name: "OpenImageIO",
     platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
-        .tvOS(.v13),
-        .watchOS(.v6)
+        .macOS(.v15),
+        .iOS(.v18),
+        .tvOS(.v18),
+        .watchOS(.v11),
+        .visionOS(.v2)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -19,7 +20,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/1amageek/OpenCoreGraphics.git", branch: "main"),
+        .package(url: "https://github.com/1amageek/OpenCoreGraphics.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
